093878412fb1a4fc847114dabd9ed612a21c90ed
dorianjagusch <dorian.jagusch@gmail.com>
2024-10-12 23:49:44 +0300

    Add task container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { TitleCasePipe} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, HeaderComponent],
+  imports: [RouterOutlet, TitleCasePipe, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  title: string = 'task-tracker';
 }
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../interfaces/Task';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskItemComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent implements OnInit{
+  tasks: Task[] = [];
+
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks=tasks);
+  }
+
+}
