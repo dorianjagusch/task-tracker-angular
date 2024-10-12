4186bebc02d36d4308beef4c6c7f0c16f6f80c66
dorianjagusch <dorian.jagusch@gmail.com>
2024-10-12 23:53:13 +0300

    Add mock-data and task service

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,14 @@
+import { Task } from './interfaces/Task.js';
+
+export const TASKS: Task[] = [
+  { id: 1, task: 'Task 1', date: new Date('2023-01-01'), reminder: true },
+  { id: 2, task: 'Task 2', date: new Date('2023-01-02'), reminder: false },
+  { id: 3, task: 'Task 3', date: new Date('2023-01-03'), reminder: true },
+  { id: 4, task: 'Task 4', date: new Date('2023-01-04'), reminder: false },
+  { id: 5, task: 'Task 5', date: new Date('2023-01-05'), reminder: true },
+  { id: 6, task: 'Task 6', date: new Date('2023-01-06'), reminder: false },
+  { id: 7, task: 'Task 7', date: new Date('2023-01-07'), reminder: true },
+  { id: 8, task: 'Task 8', date: new Date('2023-01-08'), reminder: false },
+  { id: 9, task: 'Task 9', date: new Date('2023-01-09'), reminder: true },
+  { id: 10, task: 'Task 10', date: new Date('2023-01-10'), reminder: false },
+];
